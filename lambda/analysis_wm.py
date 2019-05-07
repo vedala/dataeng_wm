2851ddc7f143bfee71cb98bebe6ef7dd1d9951a7
Sundaram Vedala <svedala23@gmail.com>
2019-05-07 13:59:13 -0400

    Lambda function for analysis 1 - wip

---
+++ lambda/analysis_wm.py
@@ -0,0 +1,45 @@
+import sys
+import logging
+import psycopg2
+import json
+import os
+
+# rds settings
+rds_host  = os.environ.get('RDS_HOST')
+rds_username = os.environ.get('RDS_USERNAME')
+rds_user_pwd = os.environ.get('RDS_USER_PWD')
+rds_db_name = os.environ.get('RDS_DB_NAME')
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+try:
+    conn_string = "host=%s user=%s password=%s dbname=%s" % \
+                    (rds_host, rds_username, rds_user_pwd, rds_db_name)
+    conn = psycopg2.connect(conn_string)
+except:
+    logger.error("ERROR: Unexpected error: Could not connect to Postgres instance.")
+    sys.exit()
+
+logger.info("SUCCESS: Connection to RDS Postgres instance succeeded")
+
+def handler(event, context):
+    """
+    This function fetches content from Postgres RDS instance
+    """
+
+    query = """select date_part('year', "Date") as year, count(1) as num_weeks
+            from (select distinct "Date" from walmartinteg.sales) tab2
+            group by 1
+            order by 1"""
+
+    with conn.cursor() as cur:
+        rows = []
+        cur.execute(query)
+        for row in cur:
+            rows.append(row)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(rows)
+    }
