70061b73e7fdd0cef21c60b5cfc91f96bc7b4b0b
Sundaram Vedala <svedala23@gmail.com>
2019-05-09 05:01:21 -0400

    Make ajax call to API gateway endpoint

--- app/dataeng_01.js
+++ app/dataeng_01.js
@@ -1,6 +1,15 @@
 function updateDivText() {
     var divElem = document.getElementById("div01");
     divElem.innerHTML = "Button clicked";
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            divElem.innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET",
+        "https://iuesc2zye0.execute-api.us-east-1.amazonaws.com/prod", true);
+    xhttp.send();
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
