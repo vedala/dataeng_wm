9531fc7e9b7d4aa4f9e0cd65e72ac38f6f7cdbdb
Sundaram Vedala <svedala23@gmail.com>
2019-05-14 13:16:42 -0400

    Analysis 2 - construct query and fetch data from database

--- lambda/analysis002.py
+++ lambda/analysis002.py
@@ -52,11 +52,14 @@ def handler(event, context):
 
     print(labels)
 
-    rows = [
-        [2010, 5000, 5200, 5400, 5600, 5800],
-        [2011, 6000, 7200, 7400, 7600, 7800],
-        [2012, 8000, 8200, 8400, 8600, 8800],
-    ]
+    query = construct_query(labels)
+    print(query)
+
+    with conn.cursor() as cur:
+        rows = []
+        cur.execute(query)
+        for row in cur:
+            rows.append(row)
 
     return_dict = {
         'statusCode': 200,
@@ -67,3 +70,93 @@ def handler(event, context):
     }
 
     return return_dict
+
+def construct_query(labels):
+    main_inner_query = """
+        (select date_part('year', "Date") as year, 'All Year' as col1, avg("Weekly_Sales")
+        from walmartinteg.sales
+        where date_part('year', "Date") in (2010, 2011, 2012)
+        group by 1, 2)
+    """
+
+    for label in labels:
+        if label == "year" or label == "All Year":
+            continue
+        elif label == "Super Bowl":
+            main_inner_query += """
+                union
+                (select date_part('year', "Date") as year, 'Super Bowl' as col1, avg("Weekly_Sales")
+                from walmartinteg.sales
+                where "Date" in ('2010-02-12', '2011-02-11', '2012-02-10')
+                group by 1, 2)
+                """
+        elif label == "Labor Day":
+            main_inner_query += """
+                union
+                (select date_part('year', "Date") as year, 'Labor Day' as col1, avg("Weekly_Sales")
+                from walmartinteg.sales
+                where "Date" in ('2010-09-10', '2011-09-09', '2012-09-07')
+                group by 1, 2)
+                """
+        elif label == "Thanksgiving":
+            main_inner_query += """
+                union
+                (select date_part('year', "Date") as year, 'Thanksgiving' as col1, avg("Weekly_Sales")
+                from walmartinteg.sales
+                where "Date" in ('2010-11-26', '2011-11-25', '2012-11-23')
+                group by 1, 2)
+                """
+        elif label == "Christmas":
+            main_inner_query += """
+                union
+                (select date_part('year', "Date") as year, 'Christmas' as col1, avg("Weekly_Sales")
+                from walmartinteg.sales
+                where "Date" in ('2010-12-31', '2011-12-30', '2012-12-28')
+                group by 1, 2)
+                """
+
+    #
+    # construct attribute list select
+    #
+    attrib_labels = []
+    for label in labels:
+        if label == "year":
+            continue
+        attrib_labels.append(label)
+
+    attribute_list_select = """select unnest(array[%s])""" % ("'" + "', '".join(attrib_labels) + "'")
+
+    #
+    # construct column definition
+    #
+    column_def_part = """mycol_def("Year" text, """
+    for label in labels:
+        if label == "year":
+            continue
+        column_def_part += "\"" + label + "\" numeric,"
+
+    column_def_part = column_def_part[:-1]
+    column_def_part += ")"
+
+    overall_query = """
+        SELECT *
+        FROM   crosstab(
+        $$
+        select * from (
+        %s
+        ) mytab order by 1,
+            case
+                when col1 = 'All Year'     then 1
+                when col1 = 'Super Bowl'   then 2
+                when col1 = 'Labor Day'    then 3
+                when col1 = 'Thanksgiving' then 4
+                when col1 = 'Christmas'    then 5
+            end
+        $$,
+        $$
+        %s
+        $$
+        ) AS %s
+        ;""" % (main_inner_query, attribute_list_select, column_def_part)
+
+    return overall_query
