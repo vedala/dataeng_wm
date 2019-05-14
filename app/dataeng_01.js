5813ec513aa6e57e9c28290207f119a753d6c333
Sundaram Vedala <svedala23@gmail.com>
2019-05-14 13:38:05 -0400

    Analysis 2 - add colors to chart

--- app/dataeng_01.js
+++ app/dataeng_01.js
@@ -156,6 +156,24 @@ function displayChart2(canvasElemName, labels, data) {
         group_labels.push(elem[0]);
     });
 
+    bgColors = [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+    ];
+
+    borderColors = [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+    ];
+
     //
     // create datasets array
     //
@@ -171,7 +189,10 @@ function displayChart2(canvasElemName, labels, data) {
         datasets_arr.push(
             {
             label: sub_group_label,
-            data: sub_group_data
+            data: sub_group_data,
+            backgroundColor: bgColors[i-1],
+            borderColor: borderColors[i-1],
+            borderWidth: 1
             }
         );
     }
