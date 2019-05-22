8c9f6036ede33adf5426de78f765a0ccc33ea09f
Sundaram Vedala <svedala23@gmail.com>
2019-05-22 09:57:38 -0400

    Create python scripts for downloading/uploading files from/to s3

---
+++ lambda/cp_to_s3.py
@@ -0,0 +1,22 @@
+import boto3
+import logging
+import os
+import sys
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+s3 = boto3.client('s3')
+
+dest_bucket_name  = os.environ.get('DEST_BUCKET_NAME')
+created_file_names  = os.environ.get('CREATED_FILE_NAMES')
+
+created_file_names_arr = created_file_names.split(',')
+for created_file_name in created_file_names_arr:
+    file_name_with_path = '/tmp/%s' % created_file_name
+    s3.upload_file(file_name_with_path, dest_bucket_name, created_file_name)
+
+logger.info("SUCCESS: Uploaded files to s3 successfully")
+
+sys.exit(0)
