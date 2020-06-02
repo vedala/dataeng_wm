0a2d017ae9b3e8a404aeb693ee5733f91ad6a67a
Kalyan Vedala <svedala23@gmail.com>
2020-06-01 21:54:23 -0700

    Update endpoint

--- app/endpoints.js
+++ app/endpoints.js
@@ -4,7 +4,7 @@ function getEndpoint(analysisId) {
         endpoint = "https://u73oq6vts0.execute-api.us-east-1.amazonaws.com/prod";
     }
     else if (analysisId === 2) {
-        endpoint = "https://psvkiegq59.execute-api.us-east-1.amazonaws.com/prod";
+        endpoint = "https://7te4r3q8lj.execute-api.us-east-1.amazonaws.com/prod";
     }
 
     return endpoint;
